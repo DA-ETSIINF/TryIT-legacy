42e80968c290381231f10a664fcf6a5744ffd3e9
alvarogtx300 <alvarogtx300@gmail.com>
2017-03-02 19:03:04 +0100

    Added contests

--- congress/urls.py
+++ congress/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', view=views.home, name='home'),
     url(r'^activities/$', view=views.activities, name='activities'),
     url(r'^workshops/$', view=views.workshops, name='workshops'),
-    # url(r'^contests/$', view=views.contests, name='contests'),
+    url(r'^contests/$', view=views.contests, name='contests'),
     url(r'^tickets/$', view=views.tickets, name='tickets'),
     url(r'^contact/$', view=views.contact, name='contact'),
     url(r'^last-editions/$', view=views.last_editions, name='last-editions'),
