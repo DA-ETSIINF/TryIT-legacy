d39448992e270aa1b59f346e59acde93385ef5e0
alvarogtx300 <alvarogtx300@gmail.com>
2018-03-05 21:24:23 +0100

    Admin of sessions more usable

--- editions/admin.py
+++ editions/admin.py
@@ -2,10 +2,32 @@ from django.contrib import admin
 
 from editions.models import Company, Edition, Speaker, SessionFormat, Session, Track, Prize
 
+
+class SessionAdmin(admin.ModelAdmin):
+    ordering = ('-start_date',)
+    list_display = ('title', 'edition', 'start_date_format', 'end_date_format')
+    list_filter = ('edition__year',)
+
+    def start_date_format(self, obj):
+        if obj.start_date is None:
+            return obj.start_date
+        return obj.start_date.strftime('%e/%m - %H:%M')
+
+    def end_date_format(self, obj):
+        if obj.end_date is None:
+            return obj.end_date
+        return obj.end_date.strftime('%e/%m - %H:%M')
+
+    start_date_format.admin_order_field = 'start_date'
+    start_date_format.short_description = 'start date'
+    end_date_format.admin_order_field = 'end_date'
+    end_date_format.short_description = 'end date'
+
+
 admin.site.register(Company)
 admin.site.register(Edition)
 admin.site.register(Speaker)
 admin.site.register(SessionFormat)
-admin.site.register(Session)
+admin.site.register(Session, SessionAdmin)
 admin.site.register(Track)
 admin.site.register(Prize)
