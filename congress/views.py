6a150d2c99134d2b5484fa44210dd19c2e13f29b
alvarogtx300 <alvarogtx300@gmail.com>
2018-03-13 01:00:06 +0100

    Fix regex url

--- congress/views.py
+++ congress/views.py
@@ -47,7 +47,7 @@ def workshops(request):
     counter = 0 #I don't know how to get index of an element of an array
     for workshop in workshops:
         description = workshop.description
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))(?![^,?.!;:])', description) #Find urls
+        urls = re.findall('http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[\/.?&+!*\-])+(?![^,!;:\s)])', description) #Find urls
         for url in urls:
             href = "<a href=\"" + url + "\">" + url + "</a>" #added ahref label(HTML)
             workshop.description = workshop.description.replace(url, href)
