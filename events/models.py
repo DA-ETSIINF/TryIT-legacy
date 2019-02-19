931e645720dc1238009f786e6d0352c92fbf124b
Daniel <mdanielm@protonmail.com>
2019-02-19 13:43:33 +0100

    Created Events models and custom admin panel

--- events/models.py
+++ events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    edition = models.ForeignKey("editions.Edition", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class EventSession(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    capacity = models.IntegerField()
+    attendants = models.ManyToManyField("tickets.Attendant")
