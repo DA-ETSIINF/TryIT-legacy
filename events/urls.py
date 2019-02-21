f05d7cf87a9ae70d4d9052e55cd2ff74824442f0
onmax <maximo.garcia.martinez@alumnos.upm.es>
2019-02-21 21:18:34 +0100

    fixed issues in form and urls

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,19 @@
 
 from django.urls import path, re_path
 from events import views
+from django.conf import settings
 
 
-urlpatterns = [
-    re_path(r'escape-room/session/(?P<pk>[0-9]+)/$', views.EscapeRoomAddAttendant.as_view(),
-            name='add-attendant'),
-    path('escape-room/', views.EscapeRoomIndexView, name='escape-room'),
-    path('escape-room/api', views.EscapeRoomSessionsView.as_view(), name='escape-room-api'),
-]
+urlpatterns = []
+
+if settings.ESCAPE_ROOM:
+    urlpatterns.append(
+        re_path(r'escape-room/session/(?P<pk>[0-9]+)/$', views.EscapeRoomAddAttendant.as_view(),
+                name='add-attendant')
+    )
+    urlpatterns.append(
+        path('escape-room/', views.EscapeRoomIndexView, name='escape-room'),
+    )
+    urlpatterns.append(
+        path('escape-room/api', views.EscapeRoomSessionsView.as_view(), name='escape-room-api')
+    )
