5cc4c493bb6a7e781577d590df2193c377af4d65
onmax <maximo.garcia.martinez@alumnos.upm.es>
2019-02-25 17:39:12 +0100

    fixed

--- congress/static/congress/angular/app.js
+++ congress/static/congress/angular/app.js
@@ -154,7 +154,7 @@
 	        const date = new Date(session.date);
 	        const hour = date.getHours();
 	        const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-	        return `${hour}:${minutes} - ${session.available}`
+	        return `${hour}:${minutes} - ${session.available} plazas`
         }
 
 	    $scope.btnSubmited = false;
